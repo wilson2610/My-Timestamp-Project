937a84aa6323ed56bb904e598e38f9244659db51
Sean Wilson <seanwilson4.sw@gmail.com>
2024-02-29 23:54:26 +0000

    Change for freeCodeCamp project

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -18,12 +19,43 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", function(req, res) {
+  var dateParam = req.params.date;
+
+  //Handle empty string passed in
+  if (dateParam === undefined) {
+    var now = new Date();
+    return res.json({ "unix": Date.parse(now), "utc": now.toUTCString() });
+  }
+  else {
+    // Check if a date passed in
+    var parsedDate = Date.parse(dateParam);
+
+    // If not a number, then it wasnt a valid date string (may be unix)
+    if (isNaN(parsedDate)) {
+      var unix = parseInt(dateParam);
+
+      if (isNaN(unix)){
+        return res.json({ error : "Invalid Date" });
+      }
+
+      var dateObject = new Date(unix);
+
+      return res.json({ "unix": unix, "utc": dateObject.toUTCString() })
+    }
+    else {
+      // parsedDate is a number so was a valid date
+      var dateObject = new Date(dateParam);
+
+      return res.json({ "unix": parsedDate, "utc": dateObject.toUTCString() });
+    }
+  }  
+});
 
 
 // Listen on port set in environment variable or default to 3000
